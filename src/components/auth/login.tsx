b88d1241c44f0762f2ac73c1d890ea2ea7e9da54
Matthew Powell <matt.powell2784@gmail.com>
2026-01-24 10:49:43 +0000

    fix: update redirect url environment variable

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleGoogleSignIn = async () => {
     setIsLoading(true);
-    const url = import.meta.env.VITE_PUBLIC_SITE_URL || import.meta.env.VITE_LOGIN_REDIRECT_URL_NETLIFY;
+    const url = import.meta.env.VITE_LOGIN_REDIRECT_URL_LOCAL || import.meta.env.VITE_LOGIN_REDIRECT_URL_NETLIFY;
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: { redirectTo: `${url}/view-quotes` },
